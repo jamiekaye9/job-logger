8ebacbfd2c3d72831141e7b8c32b7f88fa60c6e8
Jamie Kaye <jamie.kaye4@googlemail.com>
2025-05-29 20:02:23 +0100

    authentication complete

--- app/settings.py
+++ app/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect URLs after login/logout
+
+LOGIN_REDIRECT_URL = '/profile/'
+LOGOUT_REDIRECT_URL = '/'
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from jobs.forms.forms import CustomUserCreationForm
 
 def home(request):
     return render(request, 'home/home.html')
+
+def profile(request):
+    return render(request, 'profile/profile.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
